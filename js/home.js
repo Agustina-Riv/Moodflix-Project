27e8a502d3b7317480d3c72fa9e7248d6b0790fb
maylimachi <maylimachi18@gmail.com>
2026-05-31 13:05:49 -0300

    refactor: move index to root and update project paths

--- js/home.js
+++ js/home.js
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
                             <h3>${item.titulo}</h3>
                             <p>${item.tipo === 'serie' ? 'Serie' : 'Película'} • ${item.duracion} min</p>
                         </div>
-                        <a href="/pages/recomendador.html?animo=${animoBadge}" class="btn-ver-mas">Ver más</a>
+                        <a href="pages/recomendador.html?animo=${animoBadge}" class="btn-ver-mas">Ver más</a>
                     </div>
                 </div>
             `;
